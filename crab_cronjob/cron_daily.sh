972cb6449d03c2cbc8452b83bc5505c5d8efea5f
ekongat <137837876+ekongat@users.noreply.github.com>
2023-08-17 11:44:21 +0200

    Update cron_daily.sh

--- crab_cronjob/cron_daily.sh
+++ crab_cronjob/cron_daily.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 docker run --rm --net=host -v /cvmfs:/cvmfs:shared -v $HOME/workdir:/workdir \
   registry.cern.ch/cmsmonitoring/cmsmon-spark:v0.4.1.10 \
   /workdir/crab-dp3/crab_cronjob/run_spark.sh /workdir/crab-dp3/crab_cronjob/crab_data_daily.py
